6a303c5f73459e65ea4b090fd81490f75763d309
jiangtao <jiang.tao@immomo.com>
2025-02-22 22:32:53 +0800

    add print log

--- core/utils/util.py
+++ core/utils/util.py
@@ -37,6 +37,8 @@ def write_json_file(file_path, data):
 def is_segment(tokens):
     if tokens[-1] in (",", ".", "?", "，", "。", "？", "！", "!", ";", "；", ":", "："):
         return True
+    elif len(tokens) > 20:
+        return True
     else:
         return False
 
