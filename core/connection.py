9f3f591137d87a09e096d3ccf5ba5ef7bd47d4ce
jiangtao <jiang.tao@immomo.com>
2025-02-22 15:17:10 +0800

    add print log

--- core/connection.py
+++ core/connection.py
@@ -222,10 +222,10 @@ class ConnectionHandler:
             end_time = time.time()  # 记录结束时间
             if is_segment(response_message):
                 segment_text = "".join(response_message[start:]).strip()
+                self.logger.bind(tag=TAG).info(f"1,segment_text: {segment_text}")
                 segment_text = get_string_no_punctuation_or_emoji(segment_text)
-                self.logger.bind(tag=TAG).info(f"segment_text: {segment_text}")
+                self.logger.bind(tag=TAG).info(f"2,segment_text: {segment_text}")
                 if len(segment_text) > 0:
-                    self.logger.bind(tag=TAG).info(f"segment_text: {segment_text}")
                     self.recode_first_last_text(segment_text)
                     future = self.executor.submit(self.speak_and_play, segment_text)
                     self.tts_queue.put(future)
@@ -234,8 +234,9 @@ class ConnectionHandler:
         # 处理剩余的响应
         if start < len(response_message):
             segment_text = "".join(response_message[start:]).strip()
+            self.logger.bind(tag=TAG).info(f"1,segment_text: {segment_text}")
             segment_text = get_string_no_punctuation_or_emoji(segment_text)
-            self.logger.bind(tag=TAG).info(f"segment_text: {segment_text}")
+            self.logger.bind(tag=TAG).info(f"2,segment_text: {segment_text}")
             if len(segment_text) > 0:
                 self.recode_first_last_text(segment_text)
                 future = self.executor.submit(self.speak_and_play, segment_text)
